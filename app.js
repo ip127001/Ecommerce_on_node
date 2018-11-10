7831e8251f6a4d63f22e5abf73e81134956248ad
Rohit Kumawat <rkumawat125@gmail.com>
2018-11-10 20:15:28 +0530

    handling different route 'funneling'

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ const express = require('express')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware')
-    next();
+app.use('/add-product', (req, res, next) => {
+    console.log('in add product middleware')
+    res.send('<h1>hello from another route</h1>')
 })
-
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('in another middleware!');
-    res.setHeader('Content-type', 'text/html')
     res.send('<h1>Hello from express.js</h1>');
 })
-
 
 app.listen(3000);
