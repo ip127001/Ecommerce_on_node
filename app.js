2ab59fd21f6a03688516d81e977047546828445b
Rohit Kumawat <rkumawat125@gmail.com>
2018-11-10 09:57:21 +0530

    parsing request bodies using Buffer concept

--- app.js
+++ app.js
@@ -13,7 +13,21 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'dummy');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        })
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            console.log(message)
+            // const hey = "mera nam hai rohit";
+            // const result = hey.split(" ");
+            // console.log(result);
+        })
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
