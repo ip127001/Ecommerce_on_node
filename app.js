5fc78b9afe48af6e910a7f01033a8f388f54559a
Rohit Kumawat <rkumawat125@gmail.com>
2018-11-12 19:12:42 +0530

    serving files statically

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const shopRoute = require('./routes/shop');
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoute);
 app.use(shopRoute);
