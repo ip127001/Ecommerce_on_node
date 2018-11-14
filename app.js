9a8c2d600128353dfa0818ed535e8360c6705fe5
Rohit Kumawat <rkumawat125@gmail.com>
2018-11-14 11:24:17 +0530

    adding main layout to pug template

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.use('/admin', adminRoute.routes);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {
+        pageTitle: 'Error Page'
+    });
 });
 
 app.listen(3000);
