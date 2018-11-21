1323a8ef4dfe3b0abc8ef93c57940b91cd56d70c
Rohit Kumawat <rkumawat125@gmail.com>
2018-11-22 01:14:29 +0530

    read & write to/from file (data)

--- controllers/products.js
+++ controllers/products.js
@@ -15,21 +15,20 @@ exports.postAddProduct = (req, res) => {
     let newProduct = new Product(req.body.title)
     newProduct.save();
 
-    console.log(req.body.title);
+    // console.log('title', req.body.title);
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-
-    console.log(products);
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'shop',
-        path: '/shop',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'shop',
+            path: '/shop',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
     // console.log(adminData.products);
     // res.sendFile(path.join(dirName, 'views', 'shop.html'));
