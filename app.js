0aea30e4d54239c480f530824255446812c9dac9
Rohit Kumawat <rkumawat125@gmail.com>
2019-01-10 16:53:12 +0530

    products collection saved in mongodb

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', 'views');
 
 const errorController = require('./controllers/error');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 app.use(express.static(path.join(__dirname, '/public')))
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }))
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 app.use((req, res, next) => {
