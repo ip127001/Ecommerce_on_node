27f3c67b230c1c458cfb2d85e86709f2f84755eb
Rohit Kumawat <rkumawat125@gmail.com>
2019-01-10 23:53:00 +0530

    user model created

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const path = require('path');
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const errorController = require('./controllers/error');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
