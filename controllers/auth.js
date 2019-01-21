282153333bc78497699b5200c9be48aa15352d98
Rohit Kumawat <rkumawat125@gmail.com>
2019-01-22 00:34:24 +0530

    session is created on server side and unique id is saved in cookie to id the session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 exports.getLogin = (req, res, next) => {
     // const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1] === 'true';
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login/',
         pageTitle: 'Login',
@@ -8,7 +9,8 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true; Secure')
+    // res.setHeader('Set-Cookie', 'loggedIn=true; Secure')
+    req.session.isLoggedIn = true;
     res.redirect('/');
 }
 
