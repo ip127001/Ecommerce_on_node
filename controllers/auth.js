6a470cad75c237c5a8893ac2b65e2cd2794df6ab
Rohit Kumawat <rkumawat125@gmail.com>
2019-01-25 17:31:12 +0530

    issue related to order fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,16 +6,14 @@ exports.getLogin = (req, res, next) => {
     // const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1] === 'true';
     res.render('auth/login', {
         path: '/login/',
-        pageTitle: 'Login',
-        isAuthenticated: false
+        pageTitle: 'Login'
     });
 };
 
 exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
-        pageTitle: 'Signup',
-        isAuthenticated: false
+        pageTitle: 'Signup'
     });
 }
 
