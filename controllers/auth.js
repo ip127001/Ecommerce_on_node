984c5443e8d86eb066e7d8df929593d107899006
Rohit Kumawat <rkumawat125@gmail.com>
2019-01-28 22:53:03 +0530

    form values are maintained when they enter wrong input

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,7 +40,12 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
     });
 }
 
@@ -78,6 +83,17 @@ exports.postLogin = (req, res, next) => {
 
     const email = req.body.email;
     const password = req.body.password;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array()[0].msg
+        });
+    }
 
     User.findOne({
             email: email
@@ -120,7 +136,12 @@ exports.postSignup = (req, res, next) => {
         return res.status(422).render('auth/signup', {
             path: '/signup',
             pageTitle: 'Signup',
-            errorMessage: errors.array()[0].msg
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email: email,
+                password: password,
+                confirmPassword: req.body.confirmPassword
+            }
         });
     }
 
